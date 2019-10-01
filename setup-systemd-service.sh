9ea1c0f9a07037269ae469d0ed9a36223f070e2e
Andreas Langenhagen <andreas@langenhagen.cc>
2019-10-01 13:24:47 +0200

    Add call to systemctl status ping-out to setup

--- setup-systemd-service.sh
+++ setup-systemd-service.sh
@@ -9,3 +9,6 @@ sudo cp -v 'ping-out.sh' '/usr/local/bin'
 sudo cp -v 'ping-out.service' '/lib/systemd/system'
 
 printf 'systemd service ping-out installed.\n'
+
+printf 'Running\n   systemctl status ping-out\n'
+sudo systemctl status ping-out
